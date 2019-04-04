d3e556d5200edfe554f7bdc3efc275a17b277607
TOILET2000 <toilet2000@naver.com>
2019-04-04 23:56:28 +0900

    ADD - 프로젝트 추가
    - System.Collections.Generic/ListTExample
    - System.Collections.Generic/ListTKeyTValueExample

---
+++ System.Collections.Generic/DictionaryTKeyTValueExample/Program.cs
@@ -0,0 +1,15 @@
+﻿// https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.dictionary-2?view=netframework-4.7.2
+
+using System;
+
+namespace DictionaryTKeyTValueExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Type Dictionary<TKey, TValue> example here.
+        }
+    }
+}
---
+++ System.Collections.Generic/ListTExample/Program.cs
@@ -0,0 +1,14 @@
+﻿// https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=netframework-4.7.2
+
+using System;
+
+namespace ListTExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Type List<T> example here.
+        }
+    }
+}
