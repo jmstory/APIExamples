0984b86432570cabad7b0b50764e59adccbcd2b1
Toilet2000 <toilet2000@naver.com>
2019-04-10 07:44:44 +0900

    추가  - 예제 요청
    - CallerMemberNameAttribute
    - Attribute class

---
+++ System.Runtime.CompilerServices/CallerMemberNameAttributeExample/Program.cs
@@ -0,0 +1,15 @@
+﻿// https://docs.microsoft.com/en-us/dotnet/api/system.runtime.compilerservices.callermembernameattribute?view=netframework-4.7.2
+
+using System;
+
+namespace CallerMemberNameAttributeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // @전민기: 예제 추가 할 것
+        }
+    }
+}
---
+++ System/AttributeExample/Program.cs
@@ -0,0 +1,15 @@
+﻿// https://docs.microsoft.com/en-us/dotnet/api/system.attribute?view=netframework-4.7.2
+
+using System;
+
+namespace AttributeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // @전민기: 예제 추가할 것
+        }
+    }
+}
