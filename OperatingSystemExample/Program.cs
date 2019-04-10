b62d114f67d43d086ed72793177757bc422b34b2
JeonMinKi <jeonminki12@gmail.com>
2019-04-11 06:27:46 +0900

    1

---
+++ OperatingSystemExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatingSystemExample
+{
+    // ????????? 이거 왜 에러뜸?
+    // [System.Runtime.InteropServices.ComVisible(true)]
+    // [System.Serializable]
+    // public sealed class OperatingSystem : ICloneable , System.Runtime.Serialization.ISerializable
+    // {
+
+    // }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var os = Environment.OSVersion;
+            Console.WriteLine("Current OS Information:\n");
+            Console.WriteLine("Platform: {0:G}", os.Platform);
+            Console.WriteLine("Version String: {0}", os.VersionString);
+            Console.WriteLine("Version Information:");
+            Console.WriteLine("   Major: {0}", os.Version.Major);
+            Console.WriteLine("   Minor: {0}", os.Version.Minor);
+            Console.WriteLine("Service Pack: '{0}'", os.ServicePack);
+        }
+    }
+}
---
+++ System/ObsoleteAttributeExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+namespace ObsoleteAttributeExample
+{
+      //이거 왜 오류?
+//     [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct |
+//     System.AttributeTargets.Enum | System.AttributeTargets.Constructor | 
+//     System.AttributeTargets.Method | System.AttributeTargets.Property | 
+//     System.AttributeTargets.Field | System.AttributeTargets.Event | 
+//     System.AttributeTargets.Interface | System.AttributeTargets.Delegate, Inherited=false)]
+//     [System.Runtime.InteropServices.ComVisible(true)]
+//     [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Constructor |
+//     System.AttributeTargets.Delegate | System.AttributeTargets.Enum | 
+//     System.AttributeTargets.Event | System.AttributeTargets.Field | 
+//     System.AttributeTargets.Interface | System.AttributeTargets.Method |
+//     System.AttributeTargets.Property | System.AttributeTargets.Struct, Inherited=false)]
+// [   System.Serializable]
+    public sealed class ObsoleteAttribute : Attribute
+    {
+        
+
+    }
+    class Program
+    {
+        //이것도 왜 오류?
+        //[ObsoleteAttribute("This property is obsolete. Use NewProperty instead.", false)] 
+        public static string OldProperty
+        {
+            get {return "The old property value.";}
+        }
+        public static string CallOldMethod()
+        {
+            return "You have called CallOldMethod.";
+        }
+        public static string CallNewMethod() 
+        {   
+            return "You have called CallNewMethod.";
+        }   
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OldProperty);
+            Console.WriteLine();
+            Console.WriteLine(CallOldMethod());
+        }
+    }
+}
