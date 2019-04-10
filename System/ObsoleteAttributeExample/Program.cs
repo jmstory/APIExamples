38f3c26f396951fe7bdefd514b1e42257dceacfc
JeonMinKi <jeonminki12@gmail.com>
2019-04-11 07:51:58 +0900

    수정

--- System/ObsoleteAttributeExample/Program.cs
+++ System/ObsoleteAttributeExample/Program.cs
@@ -2,37 +2,24 @@
 using System.Reflection;
 
 namespace ObsoleteAttributeExample
-{
-      //이거 왜 오류?
-//     [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct |
-//     System.AttributeTargets.Enum | System.AttributeTargets.Constructor | 
-//     System.AttributeTargets.Method | System.AttributeTargets.Property | 
-//     System.AttributeTargets.Field | System.AttributeTargets.Event | 
-//     System.AttributeTargets.Interface | System.AttributeTargets.Delegate, Inherited=false)]
-//     [System.Runtime.InteropServices.ComVisible(true)]
-//     [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Constructor |
-//     System.AttributeTargets.Delegate | System.AttributeTargets.Enum | 
-//     System.AttributeTargets.Event | System.AttributeTargets.Field | 
-//     System.AttributeTargets.Interface | System.AttributeTargets.Method |
-//     System.AttributeTargets.Property | System.AttributeTargets.Struct, Inherited=false)]
-// [   System.Serializable]
-    public sealed class ObsoleteAttribute : Attribute
-    {
-        
-
-    }
+{  
     class Program
     {
-        //이것도 왜 오류?
-        //[ObsoleteAttribute("This property is obsolete. Use NewProperty instead.", false)] 
+        //경고메세지 터미널에서 확인 가능
+        [ObsoleteAttribute("This property is obsolete. Use NewProperty instead.", false)] 
         public static string OldProperty
         {
             get {return "The old property value.";}
         }
+        //경고메세지 터미널에서 확인 가능
+        [ObsoleteAttribute("This property is obsolete. Use NewProperty instead.", false)] 
         public static string CallOldMethod()
         {
             return "You have called CallOldMethod.";
         }
+
+        //사용불가로 만듬
+        //[ObsoleteAttribute("This property is obsolete. Use NewProperty instead.", true)] 
         public static string CallNewMethod() 
         {   
             return "You have called CallNewMethod.";
