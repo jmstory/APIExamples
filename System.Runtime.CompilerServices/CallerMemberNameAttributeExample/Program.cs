4bd660721177c7858a62fcb3998a9e6a896a5db3
JeonMinKi <jeonminki12@gmail.com>
2019-04-11 07:53:09 +0900

    수정

--- System.Runtime.CompilerServices/CallerMemberNameAttributeExample/Program.cs
+++ System.Runtime.CompilerServices/CallerMemberNameAttributeExample/Program.cs
@@ -9,11 +9,15 @@ namespace CallerMemberNameAttributeExample
   
    public sealed class CallerMemberNameAttribute : Attribute
    {
-        public void DoProcessing()
-        {
-            TraceMessage("Something happened");
-        }
-        public void TraceMessage(string message,
+        // public void DoProcessing()
+        // {
+        //     TraceMessage("Something happened");
+        // }
+        
+   }
+    class Program
+    {       
+       static void TraceMessage(string message,
         [System.Runtime.CompilerServices.CallerFilePath] string memberName = "",
         [System.Runtime.CompilerServices.CallerMemberName] string sourceFilePath ="",
         [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0
@@ -24,16 +28,13 @@ namespace CallerMemberNameAttributeExample
             System.Diagnostics.Trace.WriteLine("sourceFilePath: " + sourceFilePath);
             System.Diagnostics.Trace.WriteLine("sourceLineNumber: " + sourceLineNumber);
         }   
-   }
-    class Program
-    {       
-       
         
         static void Main(string[] args)
         {       
             // @전민기: 예제 추가 할 것
-            CallerMemberNameAttribute TestCallerMemberNameAttribute = new CallerMemberNameAttribute();
-            TestCallerMemberNameAttribute.DoProcessing();
+            //CallerMemberNameAttribute TestCallerMemberNameAttribute = new CallerMemberNameAttribute();
+            //TestCallerMemberNameAttribute.DoProcessing();
+            TraceMessage("aa");
         }
     }
 }
