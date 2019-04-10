5edbe67cef7be9fc90fa01af7049077bf3595f6f
JeonMinKi <jeonminki12@gmail.com>
2019-04-11 00:59:09 +0900

    에러뜸

--- System.Runtime.CompilerServices/CallerMemberNameAttributeExample/Program.cs
+++ System.Runtime.CompilerServices/CallerMemberNameAttributeExample/Program.cs
@@ -2,14 +2,38 @@
 
 using System;
 
+
 namespace CallerMemberNameAttributeExample
-{
+{    
+   [System.AttributeUsage(System.AttributeTargets.Parameter, Inherited=false)]
+  
+   public sealed class CallerMemberNameAttribute : Attribute
+   {
+       
+   }
     class Program
-    {
-        static void Main(string[] args)
+    {       
+        public void DoProcessing()
         {
-            Console.WriteLine("Hello World!");
+            TraceMessage("Something happened");
+        }
+        public void TraceMessage(string message,
+        [System.Runtime.CompilerServices.CallerFilePath] string memberName = "",
+        [System.Runtime.CompilerServices.CallerMemberName] string sourceFilePath ="",
+        [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0
+        )
+        {
+            System.Diagnostics.Trace.WriteLine("message: " + message);
+            System.Diagnostics.Trace.WriteLine("memberName: " + memberName);
+            System.Diagnostics.Trace.WriteLine("sourceFilePath: " + sourceFilePath);
+            System.Diagnostics.Trace.WriteLine("sourceLineNumber: " + sourceLineNumber);
+        }   
+      
+        static void Main(string[] args)
+        {       
             // @전민기: 예제 추가 할 것
+            
+            DoProcessing();
         }
     }
 }
